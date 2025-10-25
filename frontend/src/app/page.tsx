e018e0e3354f0d3577613521dc2d23cf2311a3aa
Noufal <noufalkakdlr@gmail.com>
2025-10-25 21:48:44 +0530

    edit navbar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="text-yellow-400">Home</div>;
+  return (
+    <div className="mt-30">
+      <div className="bg-black h-96 w-96 p-6">
+        <div className="bg-yellow-300 h-full w-full rounded-full">
+
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/src/components/SearchFilter.tsx
+++ frontend/src/components/SearchFilter.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function SearchFilter() {
   return (
-    <div className='px-10 py-5 w-80 border rounded-full border-neutral-300'>
-        where
+    <div className="flex items-center px-2 h-18 py-2  border rounded-full border-neutral-300" >
+      <div className="px-10">
+        <div className="flex gap-16">
+          <div className="mr-16">
+            <p>Where</p>
+            <p>Wanted Location</p>
+          </div>
+          <div>
+            <p>Check In</p>
+            <p>Add dates</p>
+          </div>
+          <div>
+            <p>Check Out</p>
+            <p>Add dates</p>
+          </div>
+          <div>
+            <p>Who</p>
+            <p>Add Guests</p>
+          </div>
+        </div>
+      </div>
+      <div className="h-full ">
+          <div className="bg-pink-500 w-14 h-full rounded-full" ></div>
+      </div>
+      
     </div>
-  )
+  );
 }
